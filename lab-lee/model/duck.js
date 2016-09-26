f38b6a019b48c807283c94d43807f1c17b91aad0
Lee Broxson <broxsonl@gmail.com>
2016-09-26 15:30:18 -0700

    Duckstructor made.

--- lab-lee/model/duck.js
+++ lab-lee/model/duck.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const uuid = require('uuid');
+const createError = require ('http-errors');
+const debug = require('debug')('duck:duck');
+const storage = require('../lib/storage');
+
+const Duck = module.exports = function(name, color, feathers) {
+  debug('instantiate duck');
+  if (!name) throw createError(400, 'expected name');
+  if (!color) throw createError(400, 'expected color');
+  if (!feathers) throw createError(400, 'expected num of feathers');
+
+  this.name = name;
+  this.color = color;
+  this.feathers = feathers;
+  this.id = uuid.v1();
+};
